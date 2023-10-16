2bad7e9a32657af7297c9065c4232b18cb01d94a
Illya-Bilyk <bilikillya.i@gmail.com>
2023-10-16 16:31:20 +0300

    3

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -15,7 +15,6 @@ async function getAll(req, res) {
 async function getById(req, res) {
   const { contactId } = req.params;
   const result = await getContactById(contactId);
-  console.log("getById -> result:", result);
 
   if (!result) {
     throw HttpError(404, "Not found");
@@ -46,15 +45,11 @@ async function update(req, res) {
   const { contactId } = req.params;
   const result = await updateById(contactId, req.body);
 
-  if (Object.keys(req.body).length === 0) {
-    throw HttpError(400, "missing fields");
-  }
-
   if (!result) {
     throw HttpError(404, "Not found");
   }
 
-  res.status(201).json(result);
+  res.status(200).json(result);
 }
 
 module.exports = {
